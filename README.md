ff64bab0e971a3f05198bff78c2a944acc854a29
limaia25 <limaia.25@gmail.com>
2016-05-13 12:14:16 +0100

    novo

--- README.md
+++
@@ -1,1 +0,0 @@
-# SGES_REPO
--- SGES_Manual_Utilizador/js/script.js
+++ SGES_Manual_Utilizador/js/script.js
@@ -1,6 +1,16 @@
-/* Author:
+/* Author:*/
+function refreshTabela() {
+    var tds = $(".campoObriga table");
+    for (var i = 0; i < tds.length; i++) {
+        var list = tds[i].getElementsByTagName("tr");
+        for (var j = 0; j < list.length; j++) {
+            if ((j%2)==0) {
+                $(list[j]).addClass('imparCor');
+            }
+        }
+    }
+}
 
-*/
 
 
 
