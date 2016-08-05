3a9af93c019081c0813588e94b09c8b6210d0a34
limaia25 <limaia.25@gmail.com>
2016-08-05 18:01:12 +0100

    rrr

--- SGES_Manual_Utilizador/js/script.js
+++ SGES_Manual_Utilizador/js/script.js
@@ -44,7 +44,7 @@ $('.caption').each(function () {
     var alt = $(this).attr("id");
     var text = $(this).html();
     $(this).text(textRef + n + " - " + text);
-    $("[href='" + alt + "']").each(function () {
+    $("[href='#" + alt + "']").each(function () {
         $(this).text(textRef + n);
     });
     n++;
