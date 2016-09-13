65a93c9390ba076c2b979427d8bdeb68e9522dad
limaia25 <limaia.25@gmail.com>
2016-09-13 12:03:58 +0100

    teste toPdf

--- SGES_Manual_Utilizador/js/script.js
+++ SGES_Manual_Utilizador/js/script.js
@@ -64,6 +64,43 @@ $('.caption').each(function () {
 	}
 });
 
+/**criarPDF*/
+function scriptParapdf(){
+var 
+ form = $('#content'),
+ cache_width = form.width(),
+ a4  =[ 595.28,  841.89];  // for a4 size paper width and height
+ 
+$('#create_pdf').on('click',function(){
+ $('content').scrollTop(0);
+ createPDF();
+});
+//create pdf
+function createPDF(){
+ getCanvas().then(function(canvas){
+  var 
+  img = canvas.toDataURL("image/png"),
+  doc = new jsPDF({
+          unit:'px', 
+          format:'a4'
+        });     
+        doc.addImage(img, 'JPEG', 20, 20);
+        doc.save('manual_utilizador_sges.pdf');
+        form.width(cache_width);
+ });
+}
+ 
+// create canvas object
+function getCanvas(){
+ form.width((a4[0]*1.33333) -80).css('max-width','none');
+ return html2canvas(form,{
+     imageTimeout:2000,
+     removeContainer:true
+    }); 
+}
+ 
+}());
+
 
 
 }
